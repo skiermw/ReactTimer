a6653a47c70dad9b4f0a09dc07518681bde248fc
Mark Workman <mworkman12@mchsi.com>
2017-03-13 13:53:33 -0500

    initial commit

---
+++ app/components/Nav.jsx
@@ -0,0 +1,31 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Nav = React.createClass({
+  render: function() {
+    return (
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Timer App</li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</IndexLink>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by Mark Workman
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+});
+
+module.exports = Nav;
